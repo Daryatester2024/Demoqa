26b1f88da391dc5147dd78cc949254dbb5d6b3cc
Darya <panovadaryia@gmail.com>
2025-01-16 11:45:19 +0300

    First commit

---
+++ tests/test_check_icon_demoqa.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+
+def check_icon_exist(browser):
+
+   browser.get('https://demoqa.com/')
+
+   icon=browser.find_element(By.CSS_SELECTOR,'#app > header > a')
+
+   if icon is None:
+        print('не найден')
+   else:
+        print('найден')
